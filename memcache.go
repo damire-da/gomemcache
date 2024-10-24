d9cf34f77f678279b2b88df508b0d8620a92307f
Damir.Xasanshin <damir.xasanshin@baxetgroup.com>
2024-10-24 11:05:39 +0300

    add ttl cache

--- memcache.go
+++ memcache.go
@@ -1,23 +1,45 @@
 package memcache
 
-type MemCache struct {
-	data map[string]interface{}
+import (
+	"time"
+)
+
+type CacheItem struct {
+	Value interface{}
+	Expiration int64
 }
 
-func New() MemCache {
-	return MemCache{
-		data: make(map[string]interface{}),
+type MemCache struct {
+	data map[string]CacheItem
+	ttl time.Duration
+}
+
+func New(ttl time.Duration) *MemCache {
+	return &MemCache{
+		data: make(map[string]CacheItem),
+		ttl: ttl,
 	}
 }
 
-func (c MemCache) Get(key string) interface{} {
-	return c.data[key]
+func (c *MemCache) Get(key string) (interface{}, bool) {
+	item, found := c.data[key]
+	
+	if !found {
+		return nil, false
+	}
+	
+	if time.Now().Unix() > item.Expiration {
+		delete(c.data, key)
+		return nil, false
+	}
+	
+	return item.Value, true
 }
 
 func (c *MemCache) Set(key string, value interface{}) {
-	c.data[key] = value
-}
-
-func (c *MemCache) Delete(key string) {
-	delete(c.data, key)
+	expiration := time.Now().Add(c.ttl).Unix()
+	c.data[key] = CacheItem{
+		Value:     value,
+        Expiration: expiration,
+	}
 }
